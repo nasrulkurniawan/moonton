7a95067ad57f8b5e4d0492faf7889f913fc1c2a1
nasrulkurniawan <nasrulkurniawan@gmail.com>
2022-07-22 22:06:02 +0700

    feat: Setup Spatie

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'nasrulkurniawan@gmail.com',
+            'password' => bcrypt('123456'),
+        ]);
+        $admin->assignRole('admin');
+    }
+}
